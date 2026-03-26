834db6c4d7a56b6bbef04e6d15dab1eb104a4779
NS development <imnitinyadav2552@gmail.com>
2026-03-26 15:15:14 +0530

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,5 @@
+// Replace with PropellerAds or Adsterra code
+window.onload = function() {
+    let ad = document.getElementById('ad1');
+    ad.innerHTML = "Ad will load here after you add network script";
+};
